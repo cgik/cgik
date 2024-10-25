e711219f436ce0d4071b70d2d4c343628f2199ea
chris <103071318+cgik@users.noreply.github.com>
2024-10-24 22:58:49 -0400

    nvim configs

--- .dotfiles/nvim/init.lua
+++ .dotfiles/nvim/init.lua
@@ -1,37 +1,3 @@
--- remappings
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+require("crgi")
 
-
-
--- packer plugins
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
-  use ({
-	  'rebelot/kanagawa.nvim',
-	  as = 'kanagawa',
-	  config = function()
-		  vim.cmd.colorscheme('kanagawa')
-
-		  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	  end
-  })
-
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
-  use {
-	  'nvim-treesitter/nvim-treesitter',
-	  run = function()
-		  local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-		  ts_update()
-	  end,
-  }
-end)
+vim.cmd("colorscheme kanagawa")
--- setup.sh
+++ setup.sh
@@ -1,5 +1,1 @@
 #!/bin/bash
-
-# install packer for neovim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
